71f85f2f815142cbf2290f5e7823c40855e6991e
xiang.wang <xiang.wang@yintech.cn>
2019-03-21 14:47:44 +0800

    update

--- stateService.js
+++ stateService.js
@@ -8,6 +8,7 @@
 const State = {
   status, //String 用户状态 在线|离线
   uid, //String 用户id
+  socketId, //String socket id
   userIp, //String 用户ip
   serverIp, //String 连接层服务器ip
   createdAt, //DateTime 登录时间
